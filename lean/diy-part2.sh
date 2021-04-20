1df63f0cc75936dcc245d7d2c489159fb1e3ca5c
BuildTools <unconfigured@null.spigotmc.org>
2021-04-20 22:42:29 +0800

    part2

--- lean/diy-part2.sh
+++ lean/diy-part2.sh
@@ -11,4 +11,16 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+sed -i 's/luci-theme-bootstrap/luci-theme-edge/g' feeds/luci/collections/luci/Makefile
+
+[ -e package ] && mkdir package/d -p && cd package/d
+git clone https://github.com/garypang13/luci-app-eqos.git
+git clone https://github.com/vernesong/OpenClash.git
+git clone git clone https://github.com/pymumu/luci-app-smartdns.git -b lede
+git clone https://github.com/pymumu/openwrt-smartdns.git
+git clone https://github.com/garypang13/luci-app-dnsfilter.git
+git clone https://github.com/garypang13/luci-theme-edge.git
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+git clone https://github.com/Lienol/openwrt-OpenAppFilter.git
